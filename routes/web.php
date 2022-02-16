eaef94f2518bdce731197dd06410d7c6cece15a2
Albert Einstein <kibetberniegmail.com>
2022-02-16 09:02:06 +0000

    First commit

---
+++ routes/web.php
@@ -0,0 +1,8 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Auth\RegisterController;
+Route::get('/register', [RegisterController::class,'index'])->name('register');
+Route::get('/', function () {
+    return view ('posts/index');
+});
